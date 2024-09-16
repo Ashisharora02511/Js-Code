db77b52d290ba4e573a73b66d69241476fee560b
Ashish Arora <168281150+Ashisharora02511@users.noreply.github.com>
2024-09-16 07:36:40 +0000

    dattypes in js

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,24 @@
+const accountId=122233
+let email="ashisharora555@gmail.com"
+var password=12344
+accountCity="Sirsa"
+
+//accountId=2 // not allowed
+console.log(accountId);
+email="manuarora@gmail.com";
+console.log(email);
+password=2121211
+console.log(password);
+accountCity="haryana"
+console.log(accountCity);
+let accountState;
+
+console.table([accountId,email,password,accountCity,accountState]);
+
+/*
+Prefer not to use var 
+beacuse of issue of block and functional scopes
+
+*/
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+alert("hello"); // we are using node js not browser
+let name="Ashish" // string
+let age =28; // number
+// number==> 2 to the power 53
+//bigint
+// string 
+// null ==> stand alone value 
+// undefined
+// symbol===> unique
+// object
+
