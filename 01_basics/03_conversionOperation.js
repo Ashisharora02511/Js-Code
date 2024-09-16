822ff687b1c94d3a4bc20649cbda9d656f87f2e8
Ashish Arora <168281150+Ashisharora02511@users.noreply.github.com>
2024-09-16 09:18:34 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let nums=undefined;
+console.log(typeof nums);
+let valueNums=Number(nums);
+console.log(typeof valueNums);
+console.log( valueNums);
+
+
+//"90"=> 90 easliy convert to number
+//"90asd"==> NaN
+// true ==> 1; false==>0
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+//""=>false
+// "abs"=>true
+
+let someString=90
+let StringNumber=String(someString);
+console.log(typeof someString);
+console.log(typeof StringNumber);
