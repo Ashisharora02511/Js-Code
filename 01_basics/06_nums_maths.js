18457367c48dd96fb1de8aead24cef9809e9d48b
Ashish Arora <168281150+Ashisharora02511@users.noreply.github.com>
2024-09-20 15:23:37 +0000

    dates in js

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,18 @@
+const score=400;
+console.log(score);
+const balance= new Number(4000);
+console.log(balance);
+console.log(balance.toString.length);
+console.log(balance.toFixed(3))
+const othervalue=1232.89222
+console.log(othervalue.toPrecision(3));
+const hundreds=100000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+///+++++++++++++++++++++=Maths+++++++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-9));
+console.log(Math.round(23.6))
+console.log(Math.ceil(23.7));
+console.log(Math.floor(23.8));
+
